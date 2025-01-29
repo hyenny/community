f407a27b8dd2611160b7c3a9203322d9be611cf4
hyenny <qlwb9191@naver.com>
2025-01-29 15:38:52 +0900

    feat: 인증, 인가 ExceptionHandler 추가

--- src/main/java/org/example/community/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/community/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package org.example.community.common.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,14 +16,28 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
     log.warn("handleMethodArgumentNotValidException", e);
-    ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST.value(), "유효하지 않은 입력값입니다", e.getBindingResult());
+    ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST.value(), "유효하지 않은 입력값입니다.", e.getBindingResult());
     return ResponseEntity.badRequest().body(response);
+  }
+
+  @ExceptionHandler(AuthenticationException.class)
+  public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e) {
+    log.warn("handleAuthenticationException", e);
+    ErrorResponse response = ErrorResponse.of(HttpStatus.UNAUTHORIZED.value(), "잘못된 인증 정보입니다.");
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+  }
+
+  @ExceptionHandler(AccessDeniedException.class)
+  public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e) {
+    log.warn("handleAccessDeniedException", e);
+    ErrorResponse response = ErrorResponse.of(HttpStatus.FORBIDDEN.value(), "접근 권한이 없습니다.");
+    return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
   }
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ErrorResponse> handleException(Exception e) {
     log.error("handleException", e);
-    ErrorResponse response = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+    ErrorResponse response = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버 내부 오류가 발생했습니다.");
     return ResponseEntity.internalServerError().body(response);
   }
 }
